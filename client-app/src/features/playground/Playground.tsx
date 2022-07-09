57fe463fe68931615548eb0f500fc8c576c6d8fb
Ebbe Melo Sørensen <ebs@djh75q2.usr.local>
2022-07-09 17:05:53 +0200

    -

--- client-app/src/features/playground/Playground.tsx
+++ client-app/src/features/playground/Playground.tsx
@@ -1,10 +1,37 @@
-import React from "react";
-import { Header } from "semantic-ui-react";
+import React, { Fragment } from "react";
+import { Button, Grid, Header } from "semantic-ui-react";
 
 export default function Playground() {
     return (
-        <Header as='h1'>
-            Playground
-        </Header>
+        <Fragment>
+            <Header as='h1'>
+                Playground
+            </Header>
+            <Grid>
+                <Grid.Row height='4'>
+                    <Button>
+                        Aske
+                    </Button>
+                </Grid.Row>
+                <Grid.Row height='2'>
+                    <Button>
+                        Luna
+                    </Button>
+                </Grid.Row>
+                <Grid.Column width='10'>
+                    <Button>
+                        Bamse
+                    </Button>
+                </Grid.Column>
+                <Grid.Column width='5'>
+                    <Button>
+                        Kylling
+                    </Button>
+                </Grid.Column>
+                <Button>
+                    Arthur
+                </Button>
+            </Grid>
+        </Fragment>
     )
 }
