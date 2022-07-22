0fe86b7f45336ba312f7a245f5475aebafa62a34
Ebbe Melo Sørensen <ebs@djh75q2.usr.local>
2022-07-22 20:32:01 +0200

    -

--- client-app/src/features/playground/Playground.tsx
+++ client-app/src/features/playground/Playground.tsx
@@ -1,7 +1,98 @@
+import _ from 'lodash'
 import React, { Fragment } from "react";
-import { Button, Grid, Header } from "semantic-ui-react";
+import { Image, Button, Grid, Header, Label, Icon } from "semantic-ui-react";
 
 export default function Playground() {
+    const columns = _.times(16, (i) => (
+        <Grid.Column key={i}>
+          <Image src='/assets/placeholder.png' />
+        </Grid.Column>
+      ))
+
+    const GridExampleGrid = () => <Grid>{columns}</Grid>
+
+    const GridExampleDividedNumber = () => (
+        <Grid columns={3} divided>
+          <Grid.Row>
+            <Grid.Column>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+          </Grid.Row>
+      
+          <Grid.Row>
+            <Grid.Column>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      )
+
+      const GridExampleCelled = () => (
+        <Grid celled>
+          <Grid.Row>
+            <Grid.Column width={3}>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column width={13}>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+          </Grid.Row>
+      
+          <Grid.Row>
+            <Grid.Column width={3}>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column width={10}>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+            <Grid.Column width={3}>
+                <Image src='/assets/placeholder.png' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      )
+      
+      const ButtonExampleEmphasisShorthand = () => (
+        <div>
+          <Button content='Primary' primary />
+          <Button content='Secondary' secondary />
+        </div>
+      )
+
+      const ButtonExampleAnimated = () => (
+        <div>
+          <Button animated>
+            <Button.Content visible>Next</Button.Content>
+            <Button.Content hidden>
+              <Icon name='arrow right' />
+            </Button.Content>
+          </Button>
+          <Button animated='vertical'>
+            <Button.Content hidden>Shop</Button.Content>
+            <Button.Content visible>
+              <Icon name='shop' />
+            </Button.Content>
+          </Button>
+          <Button animated='fade'>
+            <Button.Content visible>Sign-up for a Pro account</Button.Content>
+            <Button.Content hidden>$12.99 a month</Button.Content>
+          </Button>
+        </div>
+      )
+     
+      
     return (
         <Fragment>
             <Header as='h1'>
@@ -9,29 +100,35 @@ export default function Playground() {
             </Header>
             <Grid>
                 <Grid.Row height='4'>
-                    <Button>
-                        Aske
-                    </Button>
+                    <Grid.Column width='5'>
+                        <Button>
+                            Bamse
+                        </Button>
+                    </Grid.Column>
+                    <Grid.Column width='5'>
+                        <Button>
+                            Kylling
+                        </Button>
+                    </Grid.Column>
                 </Grid.Row>
-                <Grid.Row height='2'>
-                    <Button>
-                        Luna
-                    </Button>
+                <Grid.Row height='4'>
+                    <Grid.Column width='5'>
+                        <Button>
+                            Aske
+                        </Button>
+                    </Grid.Column>
+                    <Grid.Column width='5'>
+                        <Button>
+                            Luna
+                        </Button>
+                    </Grid.Column>
                 </Grid.Row>
-                <Grid.Column width='10'>
-                    <Button>
-                        Bamse
-                    </Button>
-                </Grid.Column>
-                <Grid.Column width='5'>
-                    <Button>
-                        Kylling
-                    </Button>
-                </Grid.Column>
-                <Button>
-                    Arthur
-                </Button>
             </Grid>
+            <GridExampleGrid />
+            <GridExampleDividedNumber />
+            <GridExampleCelled />
+            <ButtonExampleEmphasisShorthand />
+            <ButtonExampleAnimated />
         </Fragment>
     )
 }
