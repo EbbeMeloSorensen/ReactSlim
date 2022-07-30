37ab6452a714ae97610d950710772bcd41ae5f5e
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-07-30 07:16:08 +0200

    Removed following feature from backend

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -15,17 +15,8 @@ namespace Application.Core
                     .FirstOrDefault(x => x.IsHost).AppUser.UserName));
             CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))          
-                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName));
                 //.ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.DisplayName));
-                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.AppUser.Followers.Count))
-                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.AppUser.Followings.Count))
-                .ForMember(d => d.Following,
-                    o => o.MapFrom(s => s.AppUser.Followers.Any(x => x.Observer.UserName == currentUsername)));
-            CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
-                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count))
-                .ForMember(d => d.Following,
-                    o => o.MapFrom(s => s.Followers.Any(x => x.Observer.UserName == currentUsername)));
         }
     }
 }
