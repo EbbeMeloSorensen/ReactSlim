2564e30528767c9e0d5a8210d397387a79c0b78b
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-08-02 17:18:30 +0200

    Removed profile

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,7 +3,6 @@ import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity, ActivityFormValues } from '../models/activity';
 import { PaginatedResult } from '../models/pagination';
-import { Photo, Profile } from '../models/profile';
 import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
@@ -91,27 +90,9 @@ const Account = {
     register: (user: UserFormValues) => requests.post<User>('/account/register', user),
 }
 
-const Profiles = {
-    get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
-    uploadPhoto: (file: Blob) => {
-        let formData = new FormData();
-        formData.append('File', file);
-        return axios.post<Photo>('photos', formData, {
-            headers: {'Content-type': 'multipart/form-data'}
-        })
-    },
-    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-    updateProfile: (profile: Partial<Profile>) => requests.put(`/profiles`, profile), // Tilføjet i lesson 231 - overså du den tidligere?
-    updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
-    listFollowings: (username: string, predicate: string) =>
-        requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
-}
-
 const agent = {
     Activities,
-    Account,
-    Profiles
+    Account
 }
 
 export default agent;
