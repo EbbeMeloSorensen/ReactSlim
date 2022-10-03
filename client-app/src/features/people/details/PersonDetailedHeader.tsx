974ec7f6faab699ab8feb80705fbd9f998d764bc
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2022-10-03 16:06:13 +0200

    Working on facilitating delete from details view

--- client-app/src/features/people/details/PersonDetailedHeader.tsx
+++ client-app/src/features/people/details/PersonDetailedHeader.tsx
@@ -1,24 +1,43 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { SyntheticEvent } from 'react'
+import { Link, useHistory } from 'react-router-dom';
 import { Button, Header, Segment } from 'semantic-ui-react'
 import { Person } from "../../../app/models/person";
+import { useStore } from '../../../app/stores/store';
 
 interface Props {
     person: Person
 }
 
 export default observer (function PersonDetailedHeader({person}: Props) {
+    const history = useHistory();
+    const {personStore} = useStore();
+    const {deletePerson, loading} = personStore;
+
+    function handlePersonDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        deletePerson(id).then(() => history.push(`/people`));
+    }
+
     return (
         <Segment.Group>
             <Segment clearing attached='bottom'>
                 <Header>{person.firstName} {person.surname}</Header>
-                <Button as={Link} 
-                    to={`/manage/${person.id}`}
-                    color='orange'
-                    floated='right'>
-                    Edit
-                </Button>
+                <Button.Group floated='right'>
+                    <Button
+                        loading={loading}
+                        onClick={(e) => handlePersonDelete(e, person.id)} 
+                        color='red'
+                        type='button'>
+                        Delete
+                    </Button>
+                    <Button as={Link} 
+                        to={`/manage/${person.id}`}
+                        color='orange'
+                        floated='right'
+                        type='button'>
+                        Edit
+                    </Button>
+                </Button.Group>
             </Segment>
         </Segment.Group>
     )
